60cbc6739b1c1ca26ee7411cf8ed986a1008c16c
Frank Grytting <fgr@reknes.no>
2025-11-11 10:39:46 +0100

    fixed som linting and vite config

--- code/eslint.config.js
+++ code/eslint.config.js
@@ -1,9 +1,39 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginVitest from "eslint-plugin-vitest";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  // Base JS config
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    extends: [js.configs.recommended],
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      ...pluginReact.configs.flat.recommended.rules,
+    },
+  },
+
+  // Vitest config for test files
+  {
+    files: ["**/*.test.{js,jsx,ts,tsx}"],
+    plugins: {
+      vitest: pluginVitest,
+    },
+    rules: {
+      ...pluginVitest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...pluginVitest.environments.env.globals,
+      },
+    },
+  },
 ]);
